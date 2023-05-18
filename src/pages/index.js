efd8234d1fbbf0614b8e1857d540a2141aeda0bb
Chintanvck <20it058@charusat.edu.in>
2023-05-18 23:56:39 +0530

    8. Service structure + CSS ✅

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Style from "../style/index.module.css"
-import { HeroSection } from "@/component/componentsindex";
+import { HeroSection, Service } from "@/component/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
+      <Service/>
     </div>
   )
 };
