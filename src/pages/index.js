9accc88fcba53a155944cb341599c453ce6f7639
Chintanvck <20it058@charusat.edu.in>
2023-05-21 16:47:37 +0530

    12. Filter structure + CSS ✅

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Style from "../style/index.module.css"
-import { BigNFTSlider, Category, HeroSection, Service, Subscribe, Title } from "@/component/componentsindex";
+import { BigNFTSlider, Category, Filter, HeroSection, Service, Subscribe, Title } from "@/component/componentsindex";
 
 const Home = () => {
   return (
@@ -8,6 +8,8 @@ const Home = () => {
       <HeroSection/>
       <Service/>
       <BigNFTSlider/>
+      <Title heading="Featured NFTs" paragraph="Discover the most outstanding NFTs in all topics of life."/>
+      <Filter/>
       <Title heading="Browse by Category" paragraph="Explore the NFTs in the most featured categories."/>
       <Category/>
       <Subscribe/>
